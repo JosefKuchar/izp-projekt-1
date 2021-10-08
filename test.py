34c12ef85f94dfb3d105bb0c3b234b09741acefb
Josef Kuchař <josef.kuchar267@gmail.com>
2021-10-08 23:19:14 +0200

    Update test version

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 # Vytvoril Josef Kuchar (xkucha28) - josefkuchar.com
 # Priklad pouziti: ./test.py pwcheck
 # Help se vypise pres argument -h
-# Revize: 4
+# Revize: 5
 
 from subprocess import run, PIPE
 import sys
